d42c9b3a89af9bc35ea0863bd2e5f911e064a677
Ongezwa <ongezwaf27@gmail.com>
2022-07-12 11:11:46 +0200

    changes on local

--- sql/table.sql
+++ sql/table.sql
@@ -1,1 +1,29 @@
-CREATE TABLE
+ DROP TABLE IF EXISTS;
+
+CREATE TABLE patients(
+    id SERIAL NOT NULL PRIMARY KEY,
+    fullname VARCHAR NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password varchar(255) NOT NULL,
+    card_number VARCHAR(255)
+);
+
+ DROP TABLE IF EXISTS;
+
+
+CREATE TABLE admin(
+    id SERIAL NOT NULL PRIMARY KEY,
+    fullname VARCHAR NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password varchar(255) NOT NULL
+);
+
+ DROP TABLE IF EXISTS;
+
+CREATE TABLE appointments(
+    id SERIAL NOT NULL PRIMARY KEY,
+    date VARCHAR NOT NULL,
+    time VARCHAR NOT NULL,
+    patients_id int,
+    FOREIGN KEY (patients_id) REFERENCES patients(id)
+);
