7594f6932613f9a485dc21a574a1540fe861bfe2
OwethuSotomela <owethusotomela@gmail.com>
2022-07-18 15:34:35 +0200

    Heroku setup

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"))
 
-app.use(cors());
+app.use(cors({origin: 'http://localhost:3000'}));
 
 const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://owethusotomela:owethusotomela@localhost:5432/noQueue';
 const pgp = PgPromise({});
