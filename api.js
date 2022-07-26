586236e9717e3be77f6b4ad9181cbeec3b05d365
OwethuSotomela <owethusotomela@gmail.com>
2022-07-26 11:05:17 +0200

    Display confirmed

--- api.js
+++ api.js
@@ -195,7 +195,7 @@ module.exports = function (app, db) {
 
     app.get('/api/list', async function (req, res) {
         try {
-            const bookingBy = await db.manyOrNone(`SELECT appointments.id as id, slot, role, users_id, confirmed, description, fullname, username FROM appointments join users on appointments.users_id = users.id WHERE confirmed = 'true'`);
+            const bookingBy = await db.manyOrNone(`SELECT appointments.id as id, slot, role, users_id, confirmed, description, fullname, username FROM appointments join users on appointments.users_id = users.id WHERE confirmed = true`);
 
             res.json({
                 data: bookingBy,
