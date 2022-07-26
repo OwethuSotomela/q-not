e959802c0c545eaf5b9bb7acf8d071e7f1819953
OwethuSotomela <owethusotomela@gmail.com>
2022-07-26 14:12:35 +0200

    Confirmed

--- sql/table.sql
+++ sql/table.sql
@@ -14,7 +14,7 @@ CREATE TABLE appointments(
     id SERIAL NOT NULL PRIMARY KEY,
     slot VARCHAR NOT NULL,
     users_id int,
-    confirmed boolean default false,
+    confirmed boolean default Pending,
     description varchar(255) NOT NULL,
     FOREIGN KEY (users_id) REFERENCES users(id)
 );
