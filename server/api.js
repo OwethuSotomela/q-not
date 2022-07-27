76cb3a023fd7231aa65db3941cc564d6edadb7f7
OwethuSotomela <owethusotomela@gmail.com>
2022-07-27 10:11:56 +0200

    Authentication & authorisation

--- server/api.js
+++ server/api.js
@@ -78,6 +78,33 @@ module.exports = function (app, db) {
             })
         }
     })
+    // here 
+
+    app.get('/api/posts', verifyToken, (req, res) => {
+        jwt.verify(req.key, "secretkey", (err, authData) => {
+            if (err) {
+                res.sendStatus(403);
+            } else {
+                res.json({
+                    post: "Post created...",
+                    authData,
+                });
+            }
+        });
+    });
+
+    function verifyToken(req, res, next) {
+        const bearerHeader = req.headers["authorization"];
+        if (typeof bearerHeader !== "undefined") {
+            const bearer = bearerHeader.split(" ");
+            const bearerToken = bearer[1];
+            req.key = bearerToken;
+            next();
+        } else {
+            res.sendStatus(403);
+        }
+    }
+    // end 
 
     app.post('/api/book/:bookByDay', async function (req, res) {
         try {
