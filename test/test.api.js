c0090c1a8c0b2bbcd5912695a11b9fd95a4eb6d9
OwethuSotomela <owethusotomela@gmail.com>
2022-08-02 10:15:45 +0200

    Fixing bug

--- test/test.api.js
+++ test/test.api.js
@@ -5,7 +5,7 @@ const assert = require('assert');
 const fs = require('fs');
 require('dotenv').config()
 
-const API = require('./server/api');
+const API = require('../server/api');
 const { default: axios } = require('axios');
 const app = express();
 
