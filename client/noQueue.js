a6ef8c611c40af77a78fb502a795f8a11d5f63c1
OwethuSotomela.Owethu <OwethuSotomela.owethusotomela@gmail.com>
2022-08-16 12:30:49 +0200

    Unit testing

---
+++ client/noQueue.js
@@ -0,0 +1,526 @@
+import axios from "axios";
+import moment from 'moment';
+
+var m = moment()
+console.log(m)
+m = moment("2022-08-11T16:00:000")
+console.log(m)
+console.log(`toString() => ${m.toString()}`)
+console.log(`toISOString() => ${m.toISOString()}`)
+
+// const URL_BASE = import.meta.env.VITE_SERVER_URL;
+const URL_Heroku = "https://q-not-360-degrees.herokuapp.com";
+// const URL_Heroku = import.meta.env.VITE_SERVER_URL;
+
+const appState = {
+    Login: "LOGIN",
+    Signup: "SIGNUP",
+
+    Home: "HOME",
+
+    AdminHome: "ADMINISTRATOR",
+    Confirmation: "CONFIRMATION",
+    Manage: "MANAGE",
+    Approved: "APPROVED",
+    Schedule: "SCHEDULE"
+};
+
+export default function EQueue() {
+    return {
+        booking: [],
+        schedule: [],
+        appState: "LOGIN",
+        init() {
+            this.callFlatPicker();
+            if (localStorage["user"] !== "undefined") {
+                this.isOpen = true;
+                if (localStorage["screen"]) {
+                    this.appState = localStorage.getItem("screen");
+                } else {
+                    this.changeScreen(appState.Home);
+                }
+                if (localStorage["user"]) {
+                    this.user = localStorage.getItem("user");
+                }
+            };
+
+            this.confirmedList()
+            this.getBookings()
+            this.removeDone()
+            this.cancelsAnAppo()
+            // this.gettingUserBooking()
+            // this.getActiveStart()
+        },
+        changeScreen(name) {
+            this.appState = name;
+            localStorage.setItem("screen", name);
+            if (this.appState == appState.Confirmation) {
+                this.gettingUserBooking();
+            }
+            if (this.appState == appState.Home) {
+                this.callFlatPicker();
+            }
+            if (this.appState == appState.Approved) {
+                this.confirmedList()
+            }
+            if (this.appState == appState.Confirmation) {
+                this.callFlatPicker()
+            }
+        },
+        callFlatPicker() {
+            flatpickr(".flatpickr", {
+                enableTime: true,
+                dateFormat: "Y-m-d h:i K",
+
+                minDate: "today",
+                maxDate: "2022-11-30",
+
+                minTime: "09:00",
+                maxTime: "16:00",
+
+                inline: true,
+                weekNumbers: true,
+                allowInput: true,
+                time_24hr: true,
+
+                "disable": [
+                    function (date) {
+                        return (date.getDay() === 0 || date.getDay() === 6);
+                    },
+                    "2022-03-25",
+                    "2022-03-10",
+                    "2022-03-04",
+
+                    "2022-01-01",
+                    "2022-03-21",
+                    "2022-04-15",
+                    "2022-04-18",
+                    "2022-04-27",
+                    "2022-05-02",
+                    "2022-06-16",
+                    "2022-12-16",
+                    "2022-12-26",
+                    "2022-08-09",
+                    "2022-09-24",
+                ],
+
+                onChange(selectedDates = moment(selectedDates), dateAndTimeStr, instance) {
+                    console.log({ selectedDates, dateAndTimeStr, instance }, "on change");
+
+                    console.log(selectedDates)
+
+                    instance.config.disable.push(selectedDates[0]);
+
+                    this.booking = instance.selectedDates;
+                    console.log(this.booking)
+
+                    localStorage.setItem("Booking", this.booking);
+
+                    console.log(instance.config.disable);
+                },
+            });
+        },
+        isOpen: false,
+        feedback: "",
+        description: {
+            mhc: "Mental health care",
+            dh: "Dental Health",
+            fp: "Family Planning",
+            nbac: "NBAC",
+            art: "ART",
+            club: "Clinic for chronic diseases",
+            kids: "Child Immunization",
+        },
+        loggedIn: "Logged in as Admin:",
+        loginFeed: "",
+        user: {
+            fullname: "",
+            username: "",
+            password: "",
+            role: "",
+            id_number: "",
+            contact_number: "",
+        },
+        logUser: {
+            username: "",
+            password: ""
+        },
+        token: "",
+        loading: true,
+        description: null,
+        myBooking: [],
+        confirmedTable: [],
+        gotToSignUp() {
+            this.changeScreen(appState.Signup);
+        },
+        gotToLogin() {
+            this.changeScreen(appState.Login);
+        },
+        regUser() {
+            try {
+                const signupUser = this.user;
+                console.log(this.user)
+
+                axios
+                    .post(`${URL_Heroku}/api/register`, signupUser)
+                    .then((myApp) => {
+                        console.log(myApp.data);
+                        this.feedback = myApp.data.message;
+                        this.users = myApp.data;
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        this.feedback = err.response.data.message;
+                        setTimeout(() => {
+                            this.feedback = "";
+                        }, 3000);
+                    });
+            } catch (err) { }
+        },
+
+        login() {
+            try {
+                const loginUser = this.logUser;
+
+                axios
+                    .post(`${URL_Heroku}/api/login`, loginUser)
+                    .then((myApp) => {
+                        // console.log(myApp.data);
+                        var { access_token, user } = myApp.data;
+
+                        if (!access_token) {
+                            return false;
+                        }
+                        console.log(user)
+
+                        if (user.role == "Admin") {
+                            this.changeScreen(appState.AdminHome);
+                            this.getBookings()
+                        } else {
+                            this.changeScreen(appState.Home);
+                        }
+                        this.isOpen = true;
+                        this.user = user;
+                        localStorage.setItem("user", JSON.stringify(user));
+                        this.token = access_token;
+                        localStorage.setItem("access_token", this.token);
+                        this.loginFeed = myApp.data.message;
+                        setTimeout(() => {
+                            this.loginFeed = ""
+                        }, 3000)
+
+                        setTimeout(() => {
+                            this.loading = false;
+                        }, 1990);
+                        setTimeout(() => {
+                            this.callFlatPicker();
+
+                            this.token = "";
+                        }, 2000);
+                        return true;
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        console.log(err.response.data.message);
+
+                        setTimeout(() => {
+                            this.openLoginPopup()
+                            this.loginFeed = err.response.data.message;
+                            // this.loginFeed = 
+                        }, 1000)
+                        setTimeout(() => {
+                            this.loginFeed = "";
+                        }, 3000);
+                    });
+            } catch (err) { }
+        },
+
+        makeAnAppo() {
+            try {
+                const appoReason = this.description;
+                const bookedDay = this.Booking
+                    ? this.Booking
+                    : localStorage.getItem("Booking");
+
+                const { username } = this.user.username
+                    ? this.user
+                    : JSON.parse(localStorage.getItem("user"));
+
+                axios
+                    .post(`${URL_Heroku}/api/book/${bookedDay}`, { username, appoReason })
+                    .then((result) => result.data)
+                    .then((data) => {
+                        console.log(data);
+                    })
+                setTimeout(() => {
+                    this.openPopup()
+                }, 1000)
+
+            } catch (err) {
+                alert(err.message);
+            }
+        },
+
+        gettingUserBooking() {
+            const { username } =
+                this.user && this.user.username
+                    ? this.user
+                    : JSON.parse(localStorage.getItem("user"));
+            axios
+                .get(`${URL_Heroku}/api/booking/${username}`)
+                .then((r) => r.data)
+                .then((clinicDate) => {
+                    this.myBooking = clinicDate.data;
+                    this.user = clinicDate.user;
+
+                    localStorage.setItem("user", JSON.stringify(this.user));
+                })
+                .catch((e) => {
+                    console.log(e);
+                    // alert('Error')
+                });
+        },
+
+        cancelAppo(myAppointment) {
+            try {
+                axios
+                    .delete(`${URL_Heroku}/api/cancel/${myAppointment.id}`)
+                    .then(() => this.gettingUserBooking());
+
+                setTimeout(() => {
+                    this.opencancelPopup()
+                }, 1000)
+            } catch (err) {
+                console.log(err);
+            }
+        },
+
+        // seun 
+
+        reschedule() {
+            setTimeout(() => {
+                this.callFlatPicker();
+
+            }, 1000);
+        },
+        rescheduleAnAppo(appointments) {
+            console.log(appointments)
+            try {
+                const bookedDay = this.Booking
+                    ? this.Booking
+                    : localStorage.getItem("Booking");
+                console.log(bookedDay)
+
+                axios
+                    .post(`${URL_Heroku}/api/reschedule/${appointments.id}`, { bookedDay })
+                    .then(() => this.getBookings());
+                this.closeCalenderPopup()
+
+            } catch (err) {
+                console.log(err);
+            }
+        },
+
+        // eun 
+
+        goToConfirmation() {
+            this.changeScreen(appState.Confirmation);
+            setTimeout(() => {
+                this.gettingUserBooking();
+            }, 1000);
+        },
+        goToMakeAnAppointment() {
+            this.changeScreen(appState.Home);
+            setTimeout(() => {
+                this.callFlatPicker();
+            }, 1000);
+        },
+        goToLogin() {
+            this.changeScreen(appState.Login);
+        },
+        goToApproved() {
+            this.changeScreen(appState.Approved)
+        },
+        goToConfirm() {
+            this.changeScreen(appState.AdminHome)
+        },
+        goToSchedule() {
+            this.changeScreen(appState.Schedule)
+        },
+
+        getBookings() {
+            axios
+                .get(`${URL_Heroku}/api/booking`)
+                .then((r) => r.data)
+                .then((clinicDate) => {
+                    this.myBooking = clinicDate.data;
+                    // console.log(this.myBooking);
+                })
+                .catch((e) => {
+                    console.log(e);
+                    // alert('Error')
+                });
+        },
+
+        // Ace 
+
+        confirmAnAppo(appointments) {
+            try {
+                axios
+                    .post(`${URL_Heroku}/api/confirm/${appointments.id}`)
+                    .then(() => this.getBookings());
+                this.openConfirmPopup()
+
+            } catch (err) {
+                console.log(err);
+            }
+        },
+
+        cancelsAnAppo(appointments) {
+            try {
+                axios
+                    .post(`${URL_Heroku}/api/cancels/${appointments.id}`)
+                    .then(() => this.confirmedList());
+
+                this.feedback = "Appointment cancelled!!";
+                setTimeout(() => {
+                    this.feedback = "";
+                }, 3000)
+            } catch (err) {
+                console.log(err);
+            }
+        },
+
+        confirmedList() {
+            try {
+                axios
+                    .get(`${URL_Heroku}/api/list`)
+                    .then((r) => r.data)
+                    .then((clinicDate) => {
+                        this.confirmedTable = clinicDate.data;
+                        // console.log(this.confirmedTable);
+                    })
+                    .catch((e) => {
+                        console.log(e);
+                        // alert('Error')
+                    });
+            } catch (error) {
+                console.log(error)
+            }
+
+        },
+
+        removeDone(AllAppointment) {
+            try {
+                axios
+                    .delete(`${URL_Heroku}/api/remove/${AllAppointment.id}`)
+                    .then(() => this.confirmedList());
+
+                this.feedback = "You have removed this appointment";
+                setTimeout(() => {
+                    this.feedback = "";
+                }, 3000)
+            } catch (err) {
+                console.log(err);
+            }
+        },
+
+        // end
+
+        logout() {
+            this.isOpen = !this.isOpen;
+            this.changeScreen(appState.Login);
+            localStorage.clear();
+        },
+
+        // popup 
+        openPopup() {
+            popup.classList.add("open-popup")
+        },
+        closePopup() {
+            popup.classList.remove("open-popup")
+        },
+        opencancelPopup() {
+            cancelPopup.classList.add("open-popup")
+        },
+        closecancelPopup() {
+            cancelPopup.classList.remove("open-popup")
+        },
+        openCalenderPopup() {
+            reschedulePopup.classList.add("open-popup")
+        },
+        closeCalenderPopup() {
+            reschedulePopup.classList.remove("open-popup")
+        },
+        openLoginPopup() {
+            loginPopup.classList.add("open-popup")
+        },
+        closeLoginPopup() {
+            loginPopup.classList.remove("open-popup")
+        },
+        openConfirmPopup() {
+            confirmPopup.classList.add("open-popup")
+        },
+        closeConfirmPopup() {
+            confirmPopup.classList.remove("open-popup")
+        },
+        // end popup 
+
+        // show and hide password 
+        showPassword() {
+            var x = document.getElementById("myPassword");
+            if (x.type === "password") {
+                x.type = "text";
+            } else {
+                x.type = "password";
+            }
+        },
+        showPassword() {
+            var x = document.getElementById("myPword");
+            if (x.type === "password") {
+                x.type = "text";
+            } else {
+                x.type = "password";
+            }
+        },
+        //   end show & hide 
+        // scheduler
+
+        all() {
+            alert('All good here')
+            try {
+                axios
+                    .get(`http://localhost:5050/api/schedule`)
+                    .then((r) => r.data)
+                    .then((all) => {
+                        this.myTimeTable = all.data;
+                        console.log(this.myTimeTable);
+                    })
+                    .catch((e) => {
+                        console.log(e);
+                        // alert('Error')
+                    });
+            } catch (error) {
+                console.log(error)
+            }
+        },
+
+        createEvent(event) {
+            console.log(event)
+            alert(event.id)
+            try {
+                axios
+                    .post(`http://localhost:5050/api/event/${event.id}`)
+                    .then(result => result.data)
+                    .then((data) => {
+                        this.all()
+                        console.log(data.data)
+                    })
+            } catch (err) {
+                // alert(err.message);
+            }
+        },
+    }
+
+}
+
---
+++ server/api.js
@@ -0,0 +1,335 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcryptjs');
+
+module.exports = function (app, db) {
+
+    app.get('/api/test', function (req, res) {
+        res.json({
+            name: "OwSoto"
+        })
+    })
+
+    app.post('/api/register', async function (req, res) {
+        try {
+            const { fullname, username, password, role, id_number, contact_number } = await req.body
+
+            console.log({ fullname, username, password, role, id_number, contact_number })
+
+            console.log({ username });
+
+            if (await username == null) {
+                throw new Error("Username should be entered")
+            }
+
+            if (await password == null) {
+                throw new Error("Password should be entered")
+            }
+
+            var newUser = await db.oneOrNone("SELECT * FROM users WHERE username = $1", [username])
+
+            if (newUser !== null) {
+                throw new Error('User already exist!')
+            }
+
+            const encrypted = await bcrypt.hash(password, 10)
+
+            await db.none(`INSERT INTO users (fullname, username, password, role, id_number, contact_number) VALUES ($1, $2, $3, $4, $5, $6)`, [fullname, username, encrypted, role, id_number, contact_number]);
+            res.status(200).json({
+                message: 'New user successfully registered'
+            })
+
+        } catch (e) {
+            console.log(e.message)
+            res.status(500).json({
+                message: e.message
+            })
+        }
+    })
+
+    app.post('/api/login', async function (req, res) {
+        try {
+            const { username, password } = req.body;
+
+            const user = await db.oneOrNone(`SELECT * FROM users WHERE username = $1`, [username]);
+            console.log(user)
+
+            if (!user) {
+                throw Error('User does not exist! Register new account')
+            } else {
+                let isValidUser = await bcrypt.compare(password, user.password)
+                if (!isValidUser) {
+                    throw Error('Wrong password or username')
+                }
+                jwt.sign({ user }, 'secret', { expiresIn: '24h' }, (err, token) => {
+                    return res.json({
+                        success: true,
+                        access_token: token,
+                        user: user,
+                        message: 'Login successfull'
+                    });
+                })
+            }
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).json({
+                message: error.message
+            })
+        }
+    })
+
+    // here 
+
+    function verifyToken(req, res, next) {
+        const bearerHeader = req.headers["authorization"];
+        if (typeof bearerHeader !== "undefined") {
+            const bearer = bearerHeader.split(" ");
+            const bearerToken = bearer[1];
+            req.key = bearerToken;
+            jwt.verify(req.key, "secretkey", (err, token) => {
+                alert(token)
+                if (err) {
+                    res.sendStatus(403);
+                } else {
+                    res.json({
+                        post: "Post created...",
+                        authData,
+                        token
+                    });
+                }
+            });
+            next();
+        } else {
+            res.sendStatus(403);
+        }
+    }
+    // end 
+
+    app.post('/api/book/:bookByDay', async function (req, res) {
+        try {
+            const { username } = req.body;
+            console.log({ username })
+
+            const { bookByDay } = req.params;
+            console.log(bookByDay)
+
+            const description = req.body;
+            console.log(description)
+
+            const user = await db.oneOrNone(`SELECT * FROM users WHERE username = $1`, [username])
+            console.log({ user })
+            if (!user) {
+                throw Error('No user')
+            } else {
+
+                await db.none(`INSERT INTO appointments (slot, users_id, description) VALUES ($1, $2, $3)`, [bookByDay, user.id, description.appoReason])
+
+                res.status(200).json({
+                    message: 'A booking has been made',
+                    user
+                })
+            }
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).json({
+                error: error.message
+            })
+        }
+    })
+
+    app.get('/api/booking/:username', async function (req, res) {
+        try {
+
+            const { username } = req.params
+
+            const user = await db.oneOrNone(`SELECT * FROM users WHERE username = $1`, [username])
+
+            if (!user) {
+                console.log('No user here')
+            }
+
+            const bookingByIds = await db.manyOrNone(`SELECT * FROM appointments WHERE users_id = $1`, [user.id]);
+            console.log(bookingByIds)
+
+            res.json({
+                user: user,
+                data: bookingByIds,
+            })
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({
+                error: e.message
+            })
+        }
+    })
+
+    app.delete('/api/cancel/:id', async function (req, res) {
+
+        try {
+            const { id } = req.params;
+            await db.none(`DELETE FROM appointments WHERE id = $1`, [id])
+
+            res.json({
+                status: 'Appointment Cancelled',
+            })
+        } catch (err) {
+            console.log(err)
+            res.json({
+                status: 'Failed to cancel appointment',
+                error: err.stack
+            })
+        }
+    })
+
+    // sun 
+
+    app.post('/api/reschedule/:id', async function (req, res) {
+        try {
+            const { id } = req.params;
+
+            await db.none(`UPDATE appointments SET slot = slot + 1 WHERE id = $1`, [id])
+
+            res.status(200).json({
+                message: 'Successful',
+            })
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).json({
+                error: error.message
+            })
+        }
+    })
+    // sun end 
+
+    // Anele 
+
+    app.get('/api/booking', async function (req, res) {
+        try {
+
+            const bookingBy = await db.manyOrNone(`SELECT appointments.id as id, slot, role, users_id, id_number, status, description, fullname, username FROM appointments join users on appointments.users_id = users.id`);
+
+            res.json({
+                data: bookingBy,
+            })
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({
+                error: e.message
+            })
+        }
+    })
+
+    app.post('/api/confirm/:id', async function (req, res) {
+        try {
+
+            const { id } = req.params;
+
+            await db.none(`UPDATE appointments SET status = 'Approved' WHERE id = $1`, [id])
+
+            res.status(200).json({
+                message: 'Successful',
+            })
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).json({
+                error: error.message
+            })
+        }
+    })
+
+    app.post('/api/cancels/:id', async function (req, res) {
+        try {
+
+            const { id } = req.params;
+
+            await db.none(`UPDATE appointments SET status = 'Cancelled' WHERE id = $1 AND status = 'Approved'`, [id])
+
+            res.status(200).json({
+                message: 'Successful',
+            })
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).json({
+                error: error.message
+            })
+        }
+    })
+
+    app.get('/api/list', async function (req, res) {
+        try {
+            const bookingBy = await db.manyOrNone(`SELECT appointments.id as id, slot, role, users_id, status, description, fullname, id_number, username FROM appointments join users on appointments.users_id = users.id WHERE status = 'Approved'`);
+
+            res.json({
+                data: bookingBy,
+            })
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).json({
+                error: error.message
+            })
+        }
+    })
+
+    app.delete('/api/remove/:id', async function (req, res) {
+
+        try {
+            const { id } = req.params;
+            await db.none(`DELETE FROM appointments WHERE id = $1`, [id])
+
+            res.json({
+                status: 'Appointment Cancelled',
+            })
+        } catch (err) {
+            console.log(err)
+            res.json({
+                status: 'Failed to cancel appointment',
+                error: err.stack
+            })
+        }
+    })
+
+    // scheduler 
+
+    app.get('/api/schedule', async function (req, res) {
+
+        try {
+            const schedule = await db.manyOrNone(`SELECT * FROM events`);
+            // console.log({ schedule })
+            res.json({
+                data: schedule
+            })
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).json({
+                error: error.message
+            })
+        }
+    })
+
+    app.post('/api/event/:id', async function (req, res) {
+        try {
+            const { id } = req.params;
+
+            await db.none(`INSERT INTO events (event_id) VALUES ($1)`, [id])
+
+            res.status(200).json({
+                message: 'An event has been created!',
+                user
+            })
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).json({
+                error: error.message
+            })
+        }
+    })
+
+}
+
