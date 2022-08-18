b6770532458eedd9fbdf13905e4d5c0d490dd9fc
OwethuSotomela.Owethu <OwethuSotomela.owethusotomela@gmail.com>
2022-08-18 14:51:33 +0200

    Cors error

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"))
 
 app.use(cors({
-    origin: "https://owethusotomela.github.io"
+    header: ("Access-Control-Allow-Origin", " * ")
 }));
 
 const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://owethusotomela:owethusotomela@localhost:5432/noQueue';
