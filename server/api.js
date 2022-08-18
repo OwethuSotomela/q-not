08a3412292297a89aa18a8d919a18591c136f795
OwethuSotomela.Owethu <OwethuSotomela.owethusotomela@gmail.com>
2022-08-18 09:15:31 +0200

    Converting date

--- server/api.js
+++ server/api.js
@@ -129,12 +129,12 @@ module.exports = function (app, db) {
                 throw Error('No user')
             } else {
 
-                await db.none(`INSERT INTO appointments (slot.map(date => {
+                await db.none(`INSERT INTO appointments (slot, users_id, description) VALUES ($1, $2, $3)`, [bookByDay.map(date => {
                     return {
                         ...date,
                         slot: moment(date.slot).format('MMMM Do YYYY h:mm:ss A')
                     }
-                });, users_id, description) VALUES ($1, $2, $3)`, [bookByDay, user.id, description.appoReason])
+                }), user.id, description.appoReason])
 
                 console.log(slot)
                 res.status(200).json({
